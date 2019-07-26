a7fe5fdfc3271a605a0253cc10c1d812da193613
Zeke Hernandez <zeke.y.hernandez@gmail.com>
2019-07-26 15:26:55 +0000

    configure api invokeURL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://xo9xpmgg60.execute-api.us-east-2.amazonaws.com/zyh-prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
